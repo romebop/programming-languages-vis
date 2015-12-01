b81631b220741f87c8bf91c8d43f2e19d31bc30e
rome-bop <park.srome@gmail.com>
2015-12-01 00:21:58 -0800

    updated axis labels

--- main.js
+++ main.js
@@ -140,12 +140,12 @@ d3.json('data.json', function(error,dataset) {
     chart.append('text')
       .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
       .attr('transform', 'translate(' + -margin.left/1.5 + ',' + (height/2) + ')rotate(-90)')  // text is drawn off the screen top left, move down and out and rotate
-      .text('Number of Repositories');
+      .text('Number of Repositories (log scale)');
 
     chart.append('text')
       .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
       .attr('transform', 'translate(' + (width/2) + ',' + (height + (margin.bottom/2)) + ')')  // centre below axis
-      .text('Year');
+      .text('Year (First Appearance)');
 
   };
 
